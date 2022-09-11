d3ea45a3f48570f3f2556071d1aa3a6e9ab27e8c
CodexRodney <rodney002mwanje@gmail.com>
2022-09-11 23:31:57 +0300

    Updated file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
-/* Prints the title of a Star Wars movie where
- * the episode number matches a given integer
- * which is given in the command line
- */
-
 const request = require('request');
 
-// url to the end point used
-// url should be https://swapi-api.hbtn.io/api/films/:id
-// (id) provided in command line
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (error) console.log(error);
-  const results = JSON.parse(body); // holds the body of response
-  for (const x of results.characters) {
-    request(x, (error, response, body) => {
-      if (error) console.log(error);
-      const characters = JSON.parse(body);
-      console.log(characters.name);
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async (err, res, body) => {
+  err && console.log(err);
+
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
   }
 });
